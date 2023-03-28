395df456275a9372062f5bd6d39305b07d85d705
TVILDO2002 <72739978+TVILDO2002@users.noreply.github.com>
2023-03-28 20:27:15 +0400

    added about-part

--- static/js/app.js
+++ static/js/app.js
@@ -1,9 +1,23 @@
 const Navlinks = document.getElementById("nav-links");
 const Navburger = document.getElementById("nav-burger");
+const navleave = document.getElementById("nav-leave");
 
 function Navbaractive(){
     Navlinks.classList.toggle("nav-links__active");
     Navburger.classList.toggle("nav-burger__active");
 }
+
+let lastscrolly = window.scrollY;
+
+window.addEventListener("scroll", () => {
+    if(lastscrolly < window.scrollY) {
+        navleave.classList.add('nav--hidden');
+    }
+    else {
+        navleave.classList.remove('nav--hidden');
+    }
+
+    lastscrolly = window.scrollY;
+} );
 
 Navburger.addEventListener("click", Navbaractive);
